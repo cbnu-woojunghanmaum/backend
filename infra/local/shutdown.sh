cf2d625927c7dac3df541589ae5802a57251c665
Bangseungjae <tmdwo5825@naver.com>
2024-11-10 12:31:35 +0900

    first commit

--- infra/local/shutdown.sh
+++ infra/local/shutdown.sh
@@ -2,19 +2,19 @@
 
 echo "Shutdown zookeeper"
 
-docker-compose -f common.yml -f zookeeper.yml down
+docker compose -f common.yml -f zookeeper.yml down
 
 sleep 5
 
 echo "Shutdown kafka cluster"
 
-docker-compose -f common.yml -f kafka_cluster.yml down
+docker compose -f common.yml -f kafka_cluster.yml down
 
 sleep 5
 
 echo "Shutdown init kafka"
 
-docker-compose -f common.yml -f init_kafka.yml down
+docker compose -f common.yml -f init_kafka.yml down
 
 sleep 5
 
@@ -27,6 +27,6 @@ yes | rm -r ../zookeeper/*
 echo "Shutdown services"
 
 echo "Shutdown MongoDB and MySQL"
-docker-compose -f common.yml -f docker-compose.yml down
+docker compose -f common.yml -f docker-compose.yml down
 
 #docker compose -f common.yml -f api-module.yml down
--- infra/local/start-up.sh
+++ infra/local/start-up.sh
@@ -3,7 +3,7 @@
 echo "Starting Zookeeper"
 
 # start zookeeper
-docker-compose -f common.yml -f zookeeper.yml up -d
+docker compose -f common.yml -f zookeeper.yml up -d
 
 # check zookeeper health
 zookeeperCheckResult=$(echo ruok | nc localhost 2181)
@@ -18,7 +18,7 @@ sleep 10
 echo "Starting Kafka cluster"
 
 # start kafka
-docker-compose -f common.yml -f kafka_cluster.yml up -d
+docker compose -f common.yml -f kafka_cluster.yml up -d
 
 # check kafka health
 kafkaCheckResult=$(kcat -L -b localhost:19092 | grep '3 brokers:')
@@ -32,9 +32,9 @@ done
 echo "Creating Kafka topics"
 
 # start kafka init
-docker-compose -f common.yml -f init_kafka.yml up -d
+docker compose -f common.yml -f init_kafka.yml up -d
 
 echo "Start MySQL and MongoDB"
-docker-compose -f common.yml -f docker-compose.yml up -d
+docker compose -f common.yml -f docker-compose.yml up -d
 
 echo "Start-up completed"
